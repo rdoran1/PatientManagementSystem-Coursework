a53998a3e8faee5b5ce6f9cc86b6edf9f8020351
rdoran1 <33032112+rdoran1@users.noreply.github.com>
2020-01-08 23:31:46 +0000

    Reupload of Current Work

---
+++ PatientManagementSystem/src/patientmanagementsystem/PatientManagementSystem.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patientmanagementsystem;
+
+import GUI.GUI;
+
+/**
+ *
+ * @author rdoran1
+ */
+public class PatientManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GUI gui = new GUI();
+        gui.setVisible(true);
+        
+        
+    }
+    
+}
